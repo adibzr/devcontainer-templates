e0eb24b86970568744bd918a5f13c1af71995a5b
Adib Raed <adibraed1@gmail.com>
2025-04-09 11:26:28 -0300

    Update setup.sh to install automatically

--- react-vite-ts-template/.devcontainer/setup.sh
+++ react-vite-ts-template/.devcontainer/setup.sh
@@ -2,12 +2,12 @@
 
 if [ ! -d "my-app" ]; then
   echo "Creating Vite app..."
-  npm create vite@latest my-app --template react-ts
+  export NPM_CONFIG_YES=true
+  yes | npm create vite@latest my-app -- --template react-ts
 else
   echo "Vite app already exists."
 fi
 
-# Navigate to the app directory
 cd my-app
 echo "Installing dependencies..."
-npm install
+npm install --yes
